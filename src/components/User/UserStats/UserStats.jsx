78defe7d1d26a92e5f7b927dde13d94c2967ec14
Oleh Medvid <olmedvid@gmail.com>
2022-09-07 17:07:03 +0300

    fixed: UserStats

--- src/components/User/UserStats/UserStats.jsx
+++ src/components/User/UserStats/UserStats.jsx
@@ -1,24 +1,39 @@
 import PropTypes from 'prop-types';
 
-export const UserStats = ({stats:{followers, views, likes}}) => {
-    return (
-        <ul>
-            <li>
-                <span>Followers</span>
-                <span>{followers}</span>
+export const UserStats = ({ stats }) => {
+    const keys = Object.keys(stats); 
+ 
+    return (<ul>
+        {keys.map((key, idx) => (          
+            <li key ={idx}>
+                <span>{key}</span>
+                <span>{stats[key]}</span>
             </li>
-            <li>
-                <span>Views</span>
-                <span>{views}</span>
-            </li>
-            <li>
-                <span>Likes</span>
-                <span>{likes}</span>
-            </li>
-        </ul>
-
-    );
+        )             
+        
+    )}</ul>)
 }
+
+// export const UserStats = ({ stats: { followers, views, likes } }) => {
+//     console.log()
+//     return (
+//         <ul>
+//             <li>
+//                 <span>Followers</span>
+//                 <span>{followers}</span>
+//             </li>
+//             <li>
+//                 <span>Views</span>
+//                 <span>{views}</span>
+//             </li>
+//             <li>
+//                 <span>Likes</span>
+//                 <span>{likes}</span>
+//             </li>
+//         </ul>
+
+//     );
+// }
 
 UserStats.propTypes = {
     stats: PropTypes.exact ({
